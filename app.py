9e3243cddac74fea551888a70076b7c3720d6a19
pratheesh <yespratheesh@gmail.com>
2023-04-16 16:09:13 +0000

    until flask part 9

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request
+import os
 app = Flask(__name__) 
+app.config["UPLOAD_PATH"] = "static/images/"
 
 @app.route('/home')
 def home():
     return render_template("index.html")
-@app.route('/register',methods=['GET'])
-def register():
-    name = request.args.get("name")
-    number = request.args.get("number")
-    email = request.args.get("email")
-    return render_template("register.html",name=name,number=number,email=email)
+
 @app.route('/about')
 def about():
-    return "This is about page"
+    return render_template("about.html")
+
+@app.route('/upload',methods=["POST"])
+def upload():
+    file = request.files['file']
+    file.save(os.path.join(app.config["UPLOAD_PATH"],file.filename))
+    return "Uploaded Successfully"
+
+
+@app.route('/register',methods=['POST'])
+def register():
+    name = request.form['name']
+    number = request.form['number']
+    email = request.form['email']
+    return render_template("register.html",name=name,number=number,email=email)
+
 
 @app.route('/product')
 def product():
@@ -24,12 +36,22 @@ def laptop():
 
 @app.route('/contact')
 def contact():
-    return "This is contact page"
+    return render_template("contact.html")
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+       if request.form['username'] == 'prathat' and request.form['password'] == 'hotmail':
+           return render_template("dashboard.html")
+       else:
+           return render_template("login.html",message="Login Error")
+    else:
+       return render_template("login.html")
 
 app.add_url_rule('/',None,home)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
 
 
 
--- app_GET.py
+++ app_GET.py
@@ -1,23 +1,14 @@
 from flask import Flask, render_template, request
-import os
 app = Flask(__name__) 
-app.config["UPLOAD_PATH"] = "static/images/"
 
 @app.route('/home')
 def home():
     return render_template("index.html")
-@app.route('/upload',methods=["POST"])
-def upload():
-    file = request.files['file']
-    file.save(os.path.join(app.config["UPLOAD_PATH"],file.filename))
-    return "Uploaded Successfully"
-
-
-@app.route('/register',methods=['POST'])
+@app.route('/register',methods=['GET'])
 def register():
-    name = request.form['name']
-    number = request.form['number']
-    email = request.form['email']
+    name = request.args.get("name")
+    number = request.args.get("number")
+    email = request.args.get("email")
     return render_template("register.html",name=name,number=number,email=email)
 @app.route('/about')
 def about():
@@ -38,7 +29,7 @@ def contact():
 app.add_url_rule('/',None,home)
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
 
 
 
